6b6a53be32ee3ddda911b712c982cddd52e251fd
Louis Collard <louiscollard2@gmail.com>
2022-06-20 16:59:50 +0200

    Add days names

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,75 @@
 "use strict";
 
-// npm install dotenv and create .env
-// require("dotenv").config();
-// const apiKey = process.env.API_KEY;
-
+const apiKey = "4db1f3a4ef7796d0c3172263f48a161b";
 const btnSubmit = document.getElementById("button");
-
-const getDay = (dayIndex) => {
-	let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	return daysArray[dayIndex];
-};
 
 btnSubmit.addEventListener("click", () => {
 	const inputCity = document.getElementById("city").value;
 	async function getCityWeather() {
-		let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=metric&cnt=5`);
+		let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=metric`);
 		let data = await response.json();
 		console.log(data);
-		console.log(getDay(data.list[0].dt_txt));
+
+		// City and country
 		const cityDisplay = document.querySelector(".city");
 		const countryDisplay = document.querySelector(".country");
-		const day0neTempDisplay = document.querySelector(".day-one-temp");
-		const day0neSkyDisplay = document.querySelector(".day-one-sky");
-		const dayTwoTempDisplay = document.querySelector(".day-two-temp");
-		const dayTwoSkyDisplay = document.querySelector(".day-two-sky");
-		const dayThreeTempDisplay = document.querySelector(".day-three-temp");
-		const dayThreeSkyDisplay = document.querySelector(".day-three-sky");
-		const dayFourTempDisplay = document.querySelector(".day-four-temp");
-		const dayFourSkyDisplay = document.querySelector(".day-four-sky");
-		const dayFiveTempDisplay = document.querySelector(".day-five-temp");
-		const dayFiveSkyDisplay = document.querySelector(".day-five-sky");
 		cityDisplay.textContent = data.city.name;
 		countryDisplay.textContent = data.city.country;
-		day0neTempDisplay.textContent = data.list[0].main.temp;
-		day0neSkyDisplay.textContent = data.list[0].weather[0].main;
-		dayTwoTempDisplay.textContent = data.list[1].main.temp;
-		dayTwoSkyDisplay.textContent = data.list[1].weather[0].main;
-		dayThreeTempDisplay.textContent = data.list[2].main.temp;
-		dayThreeSkyDisplay.textContent = data.list[2].weather[0].main;
-		dayFourTempDisplay.textContent = data.list[3].main.temp;
-		dayFourSkyDisplay.textContent = data.list[3].weather[0].main;
-		dayFiveTempDisplay.textContent = data.list[4].main.temp;
-		dayFiveSkyDisplay.textContent = data.list[4].weather[0].main;
+
+		//DAY 1
+		const dateData = data.list[0].dt;
+		const dayData = new Date(dateData * 1000);
+		const dayDisplay = dayData.toDateString();
+		const dayOneNameDisplay = document.querySelector(".day-one-dayname");
+		const dayOneTempDisplay = document.querySelector(".day-one-temp");
+		const dayOneSkyDisplay = document.querySelector(".day-one-sky");
+		dayOneNameDisplay.textContent = dayDisplay;
+		dayOneTempDisplay.textContent = data.list[0].main.temp + " °C";
+		dayOneSkyDisplay.textContent = data.list[0].weather[0].main;
+
+		//DAY 2
+		const dateDataTwo = data.list[8].dt;
+		const dayDataTwo = new Date(dateDataTwo * 1000);
+		const dayDisplayTwo = dayDataTwo.toDateString();
+		const dayTwoNameDisplay = document.querySelector(".day-two-dayname");
+		const dayTwoTempDisplay = document.querySelector(".day-two-temp");
+		const dayTwoSkyDisplay = document.querySelector(".day-two-sky");
+		dayTwoNameDisplay.textContent = dayDisplayTwo;
+		dayTwoTempDisplay.textContent = data.list[8].main.temp + " °C";
+		dayTwoSkyDisplay.textContent = data.list[8].weather[0].main;
+
+		//DAY 3
+		const dateDataThree = data.list[16].dt;
+		const dayDataThree = new Date(dateDataThree * 1000);
+		const dayDisplayThree = dayDataThree.toDateString();
+		const dayThreeNameDisplay = document.querySelector(".day-three-dayname");
+		const dayThreeTempDisplay = document.querySelector(".day-three-temp");
+		const dayThreeSkyDisplay = document.querySelector(".day-three-sky");
+		dayThreeNameDisplay.textContent = dayDisplayThree;
+		dayThreeTempDisplay.textContent = data.list[16].main.temp + " °C";
+		dayThreeSkyDisplay.textContent = data.list[16].weather[0].main;
+
+		//DAY 4
+		const dateDataFour = data.list[24].dt;
+		const dayDataFour = new Date(dateDataFour * 1000);
+		const dayDisplayFour = dayDataFour.toDateString();
+		const dayFourNameDisplay = document.querySelector(".day-four-dayname");
+		const dayFourTempDisplay = document.querySelector(".day-four-temp");
+		const dayFourSkyDisplay = document.querySelector(".day-four-sky");
+		dayFourNameDisplay.textContent = dayDisplayFour;
+		dayFourTempDisplay.textContent = data.list[24].main.temp + " °C";
+		dayFourSkyDisplay.textContent = data.list[24].weather[0].main;
+
+		// DAY 5
+		const dateDataFive = data.list[32].dt;
+		const dayDataFive = new Date(dateDataFive * 1000);
+		const dayDisplayFive = dayDataFive.toDateString();
+		const dayFiveNameDisplay = document.querySelector(".day-five-dayname");
+		const dayFiveTempDisplay = document.querySelector(".day-five-temp");
+		const dayFiveSkyDisplay = document.querySelector(".day-five-sky");
+		dayFiveNameDisplay.textContent = dayDisplayFive;
+		dayFiveTempDisplay.textContent = data.list[32].main.temp + " °C";
+		dayFiveSkyDisplay.textContent = data.list[32].weather[0].main;
 	}
 	getCityWeather();
 });
